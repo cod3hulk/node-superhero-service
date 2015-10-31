4005957e8ad58644175f845a8fce3a4243c4b266
Tomas Ave <tomave82@gmail.com>
2015-10-31 14:07:19 +0100

    Added logger

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var superheroRoutes = require('./routes/superhero.js');
+var winston = require('winston');
 
+// configure app
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -10,13 +12,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(superheroRoutes);
 
+// connect db
 mongoose.connect('mongodb://192.168.59.103:32768/superhero');
 
+// start app
 var port = process.env.PORT || 3000;
 app.listen(port, err => {
   if (err) {
-    console.err(err);
+    winston.err(err);
   }
-  console.log('Listening to: ' + 'http://localhost:3000');
+  winston.info('Listening to: ' + 'http://localhost:3000');
 });
 
