25723b42de59043d007e021b41abf5acde0057d6
Tomas Ave <tomave82@gmail.com>
2015-11-04 09:28:43 +0100

    Added mockgoose and should for testing

--- test/app.js
+++ test/app.js
@@ -1,18 +1,33 @@
 var request = require('supertest');
-var assert = require('assert');
+var mongoose = require('mongoose');
+var mockgoose = require('mockgoose');
+var should = require('should');
+
+mockgoose(mongoose);
 var app = require('../src/app.js');
 
 describe('Superhero service', function () {
-  describe('GET /superheroes', function () {
-    it('should return all superheroes', function (done) {
-      request(app)
-        .get('/superheroes')
-        .expect(200)
-        .end(function(err, res) {
-          console.log(res);
-          done();
-        }); 
+    describe('POST /superheroes', function () {
+        it('should create a superhero', function (done) {
+            request(app)
+                .post('/superheroes')
+                .send({ name: "Spiderman", universe: "Marvel" })
+                .expect(201)
+                .end(function(err, res) {
+                    done();
+                }); 
+        });
     });
-  });
+    describe('GET /superheroes', function () {
+        it('should return all superheroes', function (done) {
+            request(app)
+                .get('/superheroes')
+                .expect(200)
+                .end(function(err, res) {
+                    res.body.should.have.length(1);
+                    done();
+                }); 
+        });
+    });
 });
 
